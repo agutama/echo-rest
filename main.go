26debcfe5ab7489e29a3fdd7a4440cd57aa166e4
Agus Wibawa <aguscuk@gmail.com>
2022-08-17 14:08:32 +0700

    add go-sql-driver/mysql

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/agutama/echo-rest/routes"
+import (
+	"github.com/agutama/echo-rest/db"
+	"github.com/agutama/echo-rest/routes"
+)
 
 func main() {
+	db.Init()
 	e := routes.Init()
 	e.Logger.Fatal(e.Start(":1234"))
 }
