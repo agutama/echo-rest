5dad7315ceb393402ccfdfae00fdc554be3f60ed
Agus Wibawa <aguscuk@gmail.com>
2022-08-17 19:35:21 +0700

    add jwt token

--- controllers/login.controller.go
+++ controllers/login.controller.go
@@ -2,9 +2,11 @@ package controllers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/agutama/echo-rest/helpers"
 	"github.com/agutama/echo-rest/models"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo/v4"
 )
 
@@ -23,7 +25,25 @@ func CheckLogin(c echo.Context) error {
 		return echo.ErrUnauthorized
 	}
 
-	return c.String(http.StatusOK, "Berhasil Login.")
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["username"] = username
+	claims["level"] = "application"
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("bismillah123"))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"message": err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"token": t,
+	})
+
+	// return c.String(http.StatusOK, "Berhasil Login.")
 }
 
 func GenerateHashPassword(c echo.Context) error {
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require github.com/labstack/echo/v4 v4.8.0
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
