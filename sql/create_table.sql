8552da5776912d888979fe009ac2b3d8e522d754
Agus Wibawa <aguscuk@gmail.com>
2022-08-19 15:03:30 +0700

    refactoring

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,12 +1,3 @@
-CREATE TABLE public.pegawai (
-	id int4 NOT NULL,
-	nama varchar(100) NOT NULL,
-	alamat text NULL,
-	telepon varchar(15) NULL,
-	CONSTRAINT pegawai_id PRIMARY KEY (id)
-);
-
-
 CREATE TABLE public.users (
 	id int4 NOT NULL,
 	username varchar(100) NOT NULL,
